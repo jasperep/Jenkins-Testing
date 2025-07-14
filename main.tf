3b82cc1c81a496b10ec82a3fb3c38bd9fe276b2a
Jasper Palmer <jasperep@amazon.com>
2025-07-14 14:55:46 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_sqs_queue" "example" {
+  name                      = "example"
+  delay_seconds             = 10
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
